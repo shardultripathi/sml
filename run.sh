7b09bcccdc8fede6123bf1ba34d16c5b3c13fe68
Shardul Tripathi <shardul.511@gmail.com>
2017-07-04 21:07:02 +0530

    improved run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ cd java
 java -jar /usr/local/lib/antlr-4.7-complete.jar sml.g4
 javac *.java
 java org.antlr.v4.gui.TestRig sml commandSeq -gui < ../input.txt
+rm *class
 cd ..
 java -jar /usr/local/lib/antlr-4.7-complete.jar -Dlanguage=Python3 sml.g4 -visitor
 # python3 sml.py input.txt
